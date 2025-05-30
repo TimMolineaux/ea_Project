72cdda54a27164a26e03b2cd7f7a88968a7d8aa7
TimMolineaux <tim.molineaux@gmail.com>
2025-05-31 01:11:57 +0200

    intitiele commit

---
+++ src/main/java/org/example/ea_project/EaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.ea_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ea_project/EaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ea_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
