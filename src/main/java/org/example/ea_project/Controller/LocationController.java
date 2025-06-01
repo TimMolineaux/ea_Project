4184a52d94e2a08dcefcc8a422961727f17fecc2
TimMolineaux <tim.molineaux@gmail.com>
2025-06-01 17:23:50 +0200

    Location controller functies om locaties toe te voegen en verwijderen gegeven

--- src/main/java/org/example/ea_project/Controller/LocationController.java
+++ src/main/java/org/example/ea_project/Controller/LocationController.java
@@ -3,9 +3,10 @@ package org.example.ea_project.Controller;
 import org.example.ea_project.Model.Location;
 import org.example.ea_project.Model.LocationDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/location")
@@ -21,6 +22,28 @@ public class LocationController {
     @GetMapping
     public Iterable<Location> getAllLocations() {
         return locationDAO.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Location> getLocationById(@PathVariable int id) {
+        Optional<Location> location = locationDAO.findById(id);
+        return location.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location) {
+        return locationDAO.save(location);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLocation(@PathVariable int id) {
+        if (locationDAO.existsById(id)) {
+            locationDAO.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- src/main/java/org/example/ea_project/Model/Event.java
+++ src/main/java/org/example/ea_project/Model/Event.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 public class Event {
